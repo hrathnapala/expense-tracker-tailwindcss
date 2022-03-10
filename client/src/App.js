31e75715d04fb8673f933aa63644afd2d4fd0e36
Heshan Rathnapala <hrathnapala@gmail.com>
2022-03-10 17:27:13 +0530

    Small change in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ const App = () => {
     <div className="body">
       <div className="container mx-auto max-w-6xl drop-shadow-lg text-gray-800">
         <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">
-          Expense Tracker
+          Expense Tracker App
         </h1>
 
         {/* grid columns */}
